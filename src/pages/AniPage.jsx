4d717b55f91341a7c4c502504b7c513c6bd060d5
devyubi <dev.munyubi@gmail.com>
2025-08-12 11:57:59 +0900

    [docs] animejs 기본 활용

--- src/pages/AniPage.jsx
+++ src/pages/AniPage.jsx
@@ -1,15 +1,3 @@
-# Anime.js
-
-- https://animejs.com/
-- https://velog.io/@bluestragglr/Anime.js-사용하기
-- https://www.npmjs.com/package/animejs
-- 설치 : `npm i animejs`
-
-## 폴더 및 파일 구성
-
-- /src/pages/AniPage.jsx
-
-```jsx
 import { animate, createDraggable } from "animejs";
 import { useEffect, useRef } from "react";
 
@@ -85,4 +73,3 @@ function AniPage() {
   );
 }
 export default AniPage;
-```
